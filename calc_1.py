d666f6273652345b9ffa6180f35a98efd3a9d59d
igor <headmx1989@gmail.com>
2021-09-02 13:50:05 +0300

    change calc_1.py

--- calc_1.py
+++ calc_1.py
@@ -1,28 +1,37 @@
 print ('Калькулятор приветсвует Вас!')
-a = 1
-while a == 1 :
-    number1 = float(input('введите 1 число: '))
-    number2 = float (input('введите 2 число: '))
-    operation = input('что будем делать(+/-*)? ')
-    if operation == '/' and number2 == 0:
-        print ('на ноль делить нельзя!')
-    elif operation != '+' and operation != '-' and operation != '*' and operation != '/':
-        print ('Этого делать мы не умеем пока, используйте */-+')
-        continue  
-    elif operation == '-':
-        print (number1 - number2)
-    elif operation == '+':
-        print (number1 + number2)
-    elif operation == '/':
-        print (number1 / number2)
-    elif operation == '*':
-        print (number1 * number2)
+question = 'y'
+while question == 'y' :
+    number1 = float(input(' '))
+    operation = input(' ')
+    temporary = number1
+    while True:
+        number1 = temporary
+        number2 = float (input(' '))
+        if operation == '/' and number2 == 0:
+            print ('на ноль делить нельзя!')
+            break
+        elif operation != '+' and operation != '-' and operation != '*' and operation != '/':
+            print ('Этого делать мы не умеем пока, используйте */-+')
+            break  
+        elif operation == '-':
+            temporary = number1 - number2
+        elif operation == '+':
+            temporary = number1 + number2
+        elif operation == '/':
+            temporary = number1 / number2
+        elif operation == '*':
+            temporary = number1 * number2
+        operation = input ('')
+        if operation == '=':
+            print (temporary)
+            break
+        else:
+            continue    
     question = input ('продолжаем y/n ? ')
     if question == 'y':
         continue
     else:
         print ('до свидания')
         break    
-else:
-    print ('что-то не то')
+
    
