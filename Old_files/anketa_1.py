ecc95238764505c6e0476c14aef81470dd6da523
igor <headmx1989@gmail.com>
2021-09-05 16:10:29 +0300

    clear old_files

---
+++ Old_files/anketa_1.py
@@ -0,0 +1,5 @@
+first_name = input('Введите свое имя: ')
+last_name = input('введите фамилию: ')
+birtday= int(input('ваш год рождения:'))
+age = 2021 - birtday
+print('Здравствуйте, меня зовут', first_name + last_name, 'мне ', age, )
