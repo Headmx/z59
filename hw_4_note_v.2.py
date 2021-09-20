3dc9fd2e69854e38bf226c72f6ce242f1b6e0669
igor <headmx1989@gmail.com>
2021-09-20 13:24:01 +0300

    добавлен little_func.py (сделано 3 из 7), обновлен hw_4_notev.2.py (добавлена возможность сохранения и загрузки с файла, также не работают 7 и 9)

--- hw_4_note_v.2.py
+++ hw_4_note_v.2.py
@@ -1,4 +1,5 @@
 print ('Записная приветствует Вас')
+import json
 todos = {}
 
 def strike(text):
@@ -9,7 +10,7 @@ def strike(text):
 
 def strike_todo():
     while True:
-        number = int(input ('какой номер задачи отмечаем:\n '))
+        number = input ('какой номер задачи отмечаем:\n ')
         text = todos[number] 
         todos[number] = strike(text)
         y = input('желаете продолжить? (y/n) ')
@@ -17,7 +18,7 @@ def strike_todo():
             continue
         else:
             break
-    return print_todos(todos)
+    return print_todos()
 
 def strike_all ():
     while True:
@@ -26,7 +27,7 @@ def strike_all ():
             todos[key] = strike(text)
         else:
             break
-    return print_todos(todos)
+    return print_todos()
 
 def valid_number (number):
     return number.isdigit() and 0<int(number)<= len(todos)
@@ -34,55 +35,81 @@ def valid_number (number):
 def add_todo():
     while True:
         todo = input('Введите задачу:')
-        i = len(todos) + 1
+        i = str(len(todos) + 1)
         todos[i] = todo
         y = input('желаете продолжить? (y/n) ')
         if y in ('y', 'Y'):
             continue
         else:
             break
-    return print_todos(todos)
+    return print_todos()
 
 def del_todo ():
     while len(todos):
-        print_todos (todos)
-        number = int(input ('введите номер задачи:'))
-        todos.pop(number)
+        print_todos ()
+        number = input ('введите номер задачи:')
+        todos.pop(number)        
         y = input('желаете продолжить? (y/n) ')
         if y in ('y', 'Y'):
             continue
         else:
             break
-    return print_todos(todos)
+    return print_todos()
 
-def print_todos(dict_1):
-    for key, value in dict_1.items(): 
+def print_todos():
+    global todos
+    for key, value in todos.items(): 
         print (key,  value)
     return 
 
 def update_todo():
     while True:
-        print_todos (todos)
+        print_todos ()
         number = input ('введите номер задачи:')
-        if not valid_number (number):
+        if number not in todos.keys():
             print (' нет такого номера, пробуйте еще:\n')
             continue
         else:
             text = input ('введите новую задачу:\n')
-            todos[int(number)] = text    
+            todos[number] = text    
             y = input('желаете продолжить? (y/n) ')
             if y in ('y', 'Y'):
                 continue
             return 
 
 def filter_todos(text):
-    return filter(lambda x: text in x ['text'], todos)
-        
-        
-        
+    d = {key: value for key, value in todos.items() if text in value['text']}
+    return list(d)
 
+def open_todos():
+    add_file = input('введите имя файла своей todo, или n:')
+    if add_file in ('N','n','No','no','NO', False, ''):
+        global todos
+        with open ('my_todo.json', 'r') as todos_json:
+            todos = json.load(todos_json)
+    else:
+        with open (f'{add_file}', 'r') as todos_json:
+            todos = json.load(todos_json)
+    return print_todos
 
+def save_todos():
+    save_file = input('в какой файл сохраняем')
+    if save_file in ('N','n','No','no','NO', False, ''):
+        with open('my_todo.json', 'w') as todos_json:
+            json.dump(todos,todos_json)
+            return print ('Saved in my_todos.json' )
+    else:
+        with open(f'{save_file}', 'w') as todos_json:
+            json.dump(todos,todos_json)
+            return print (f'Saved in {save_file}' )
+    
 
+def all_del_todos():
+    y = input('точно удаляем? (y/n) ')
+    if y in ('y', 'Y'):
+        todos.clear()
+      
+    
 menu = '''
 1. посмотреть задачи
 2. добавить задачу
@@ -96,11 +123,11 @@ menu = '''
 0. выйти
 ''' 
 
-
+open_todos()
 while True:
     choice = input(menu)
     if choice == '1':
-        print_todos(todos)
+        print_todos()
     elif choice == '2':
         add_todo()
     elif choice == '3':
@@ -114,13 +141,12 @@ while True:
     elif choice == '7':
         break
     elif choice == '8':
-        todos.clear()
-        continue
+        all_del_todos()
     elif choice == '9':
         text = input ('что ищем? \n:')
-        a = filter (filter_todos,todos)
-        print (list (a))
+        filter_todos(text)     
     elif choice == '0':
+        save_todos()
         print ("Goodbye!")
         break
     else:
